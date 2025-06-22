e75e6b08fbeb0b1ed96cd5f2fc3cf95db97a479d
Julia Marushchenko <marushchenko.yulia@gmail.com>
2025-06-22 19:09:21 +0200

    Add files via upload
    Committing Main class with LinkedList

---
+++ src/com/list/Main.java
@@ -0,0 +1,56 @@
+// Java program to demonstrate java ArrayList
+
+package com.list;
+
+import java.util.LinkedList;
+import java.util.List;
+
+// Main class
+public class Main {
+
+    // Main method to run java program
+    public static void main(String[] args) {
+
+        // Creating LinkedList
+        List<String> list = new LinkedList<>();
+
+        // Adding elements to the list
+        list.add("Adidas");
+        list.add("Nike");
+        list.add("Tschibo");
+        list.add("Colistar");
+        list.add("Chanel");
+        list.add("Sisly");
+        list.add("Elizabeth Arden");
+        list.add("Victoria's Secret");
+        list.add("Skiny");
+        list.add("Snipet");
+        list.add("Marc O'Polo");
+        list.add("Michael Kors");
+        list.add("Starbucks");
+
+        // Printing the list to console
+        System.out.println(list);
+
+        // Checking if list contains Victoria's Secret
+        System.out.println("Is there Victoria's Secret: " + list.contains("Victoria's Secret"));
+
+        // Checking if list contains Volvo
+        System.out.println("Is there Victoria's Volvo: " + list.contains("Volvo"));
+
+        // Remove elemnt Michael Kors
+        list.remove("Michael Kors");
+
+        // Printing the list to console
+        System.out.println(list);
+
+        // Remove elemnt Tschibo
+        list.remove("Tschibo");
+
+        // Printing the list to console
+        System.out.println(list);
+
+        // Checking if list contains Tschibo
+        System.out.println("Is there Tschibo: " + list.contains("Tschibo"));
+    }
+}
